6811827043f1d61432bbeb0b217edc3c1866f5ee
ulviabdullazada <ulvi.abdullazada@code.edu.az>
2024-05-06 13:48:51 +0400

    MVC front to back

---
+++ FiuApp/FiuApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiuApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ FiuApp/FiuApp/Program.cs
@@ -0,0 +1,21 @@
+namespace FiuApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                "default",
+                "{controller=Home}/{action=Index}"
+
+                );
+
+            app.Run();
+        }
+    }
+}
